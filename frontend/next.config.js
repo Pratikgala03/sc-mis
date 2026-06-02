1483b3bcf38f626eae5a5eced625253bc403cd80
Pratikgala03 <pratikgala03@gmail.com>
2026-06-02 21:16:13 +0100

    Add frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    if (process.env.NEXT_PUBLIC_API_URL) return [];
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
